c75cebdd50db9ef3c0f7f5460db4d6e933387a18
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-24 12:55:13 +0500

    Add files via upload

---
+++ assignment_08.js
@@ -0,0 +1,13 @@
+// Q8 You should create four lines that look like this:
+// -------------------------
+// console.log(5 + 3)
+// -------------------------
+// console.log("--------------------");
+// console.log(4+4);
+// console.log("--------------------");
+// console.log("====================");
+// console.log(12-4);
+// console.log("====================");
+// console.log("____________________");
+// console.log(4*2);
+// console.log("____________________");
