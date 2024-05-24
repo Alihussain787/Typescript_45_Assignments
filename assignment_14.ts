401647492edb60ffb4f4aa5983abe7511977a0be
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-24 16:35:36 +0500

    Add files via upload

---
+++ assignment_14.ts
@@ -0,0 +1,9 @@
+/* Q14 Guest List: If you could invite anyone, living or deceased, to dinner, 
+who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+Then use your list to print a message to each person, inviting them to dinner.*/
+
+// let guest = ["Ali","Khaleel","Farhan"];
+
+// console.log(`Dear ${guest[0]} please come to dinner.`);
+// console.log(`Dear ${guest[1]} please come to dinner.`);
+// console.log(`Dear ${guest[2]} please come to dinner.`);
