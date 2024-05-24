1b2ace1e7619c38068606672accfd7feec1c545c
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-24 16:59:58 +0500

    Add files via upload

---
+++ assignment_15.ts
@@ -0,0 +1,24 @@
+/* Q15 Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+• Start with your program from Exercise 14. 
+Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+• Modify your list, replacing the name of the guest who can’t make it 
+with the name of the new person you are inviting.
+
+• Print a second set of invitation messages, one for each person who is still in your list.*/
+
+// let guest = ["Ali","Khaleel","Farhan"];
+
+// console.log(`Dear ${guest[0]} please come to dinner.`);
+// console.log(`Dear ${guest[1]} please come to dinner.`);
+// console.log(`Dear ${guest[2]} please come to dinner.`);
+
+// console.log(`${guest[1]} can not come to dinner`);
+
+// let replacing = guest.splice(1,1,"Aliza");// replace new guest in the list
+
+// for(let i = 0; i < guest.length; i++){
+//     console.log(`Dear ${guest[i]} please come to dinner`);
+// }
