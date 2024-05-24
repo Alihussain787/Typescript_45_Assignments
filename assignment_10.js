543e04d7634eadc27a5170ff19db3e9027d896c6
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-24 14:58:25 +0500

    Add files via upload

---
+++ assignment_10.js
@@ -0,0 +1,14 @@
+/*Q10 Adding Comments: Choose two of the programs you’ve written,
+and add at least one comment to each.
+If you don’t have anything specific to write because your programs are too simple at this point,
+just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.*/
+
+// This program concatenate two strings
+// declare two variables and assign string values
+// let first_Name = "Ali";
+// let last_Name = "Hussain";
+// concatenate two strings using plus sign
+// let result = first_Name + last_Name;
+// print result
+// console.log(result);
