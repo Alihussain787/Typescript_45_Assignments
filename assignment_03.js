6cc6c46c63cc1e1657c7c880d79a71c0a20a5093
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-24 11:56:19 +0500

    Add files via upload

---
+++ assignment_03.js
@@ -0,0 +1,10 @@
+// Q3 Name Cases: Store a person’s name in a variable, 
+//and then print that person’s name in lowercase, uppercase, and titlecase.
+// let person_Name = "Ali hussain soomro";
+// // to lowercase
+// console.log(person_Name.toLowerCase());
+// // to uppercase
+// console.log(person_Name.toUpperCase());
+// // to titlecase
+// let titleCaseName = person_Name.split(" ").map((word:string) => word[0].toUpperCase() + word.slice(1)).join(" ");
+// console.log(titleCaseName);
