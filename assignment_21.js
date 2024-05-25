fd46b99572b64f7480105e6737265473b57c26bb
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-25 10:09:51 +0500

    Add files via upload

---
+++ assignment_21.js
@@ -0,0 +1,21 @@
+/*Q21 They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items.*/
+var student = {
+    name: "Ali",
+    f_name: "M.Lqiue",
+    age: 24
+};
+console.log(student);
+var employee = {
+    name: "Ahmed",
+    salary: "15000",
+    id: 21
+};
+console.log(employee);
+var car = {
+    model: 2024,
+    name: "mehran",
+    color: "white",
+    price: 1500000
+};
+console.log(car);
---
+++ assignment_21.ts
@@ -0,0 +1,24 @@
+/*Q21 They think of something you could store in a TypeScript Object. 
+Write a program that creates Objects containing these items.*/ 
+
+let student = {
+    name:"Ali",
+    f_name:"M.Lqiue",
+    age:24
+}
+console.log(student);
+
+let employee = {
+    name:"Ahmed",
+    salary:"15000",
+    id:21
+}
+console.log(employee);
+
+let car = {
+    model:2024,
+    name:"mehran",
+    color:"white",
+    price:1500000
+}
+console.log(car);
