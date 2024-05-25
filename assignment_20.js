c482309732177db966926297f95e4bb0d400bdc4
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-25 09:55:24 +0500

    Add files via upload

---
+++ assignment_20.js
@@ -0,0 +1,14 @@
+/* Q20 Think of something you could store in a array.
+For example, you could make a list of mountains,
+rivers, countries, cities, languages, or anything else you’d like.
+Write a program that creates a list containing these items.*/
+// array list of countries
+var countries = ["Pakistan", "Iran", "Afganistan", "UAE", "Saudi Arabia"];
+// array list of cities
+var cities = ["Karachi", "Hyderabad", "Lahore", "Islamabad", "Peshawar"];
+// array list of languages
+var languages = ["Urdu", "Sindhi", "Arabic", "Pashto", "Balochi"];
+// array list of colors
+var colors = ["Red", "Green", "Black", "White", "Yellow", "Blue"];
+// array list of mobiles
+var mobiles = ["Iphone", "Oppo", "Samsung"];
---
+++ assignment_20.ts
@@ -0,0 +1,20 @@
+/* Q20 Think of something you could store in a array. 
+For example, you could make a list of mountains, 
+rivers, countries, cities, languages, or anything else you’d like. 
+Write a program that creates a list containing these items.*/
+
+
+// array list of countries
+let countries = ["Pakistan","Iran","Afganistan","UAE","Saudi Arabia"];
+
+// array list of cities
+let cities = ["Karachi","Hyderabad","Lahore","Islamabad","Peshawar"];
+
+// array list of languages
+let languages = ["Urdu","Sindhi","Arabic","Pashto","Balochi"];
+
+// array list of colors
+let colors = ["Red","Green","Black","White","Yellow","Blue"];
+
+// array list of mobiles
+let mobiles = ["Iphone","Oppo","Samsung"];
