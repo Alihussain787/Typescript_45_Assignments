8bf1d7c5ab693c4b2d867ef65d6421d2a872fabc
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-25 10:56:10 +0500

    Add files via upload

---
+++ assignment_23.js
@@ -0,0 +1,38 @@
+/*Q23 Conditional Tests: Write a series of conditional tests.
+Print a statement describing each test and your prediction for the results of each test.
+Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. */
+var value = 10;
+console.log("value == 10? I predict true");
+console.log(value == 10); // true
+console.log("value == 11? I predict false");
+console.log(value == 11); // false
+var student = "Ali";
+console.log("student == 'Ali'? I predict true");
+console.log(student == 'Ali'); // true
+console.log("student == 'Ameen'? I predict false");
+console.log(student == 'Ameen'); // false
+var teacher = "Ameen";
+console.log("teacher == 'Ameen'? I predict true");
+console.log(teacher == 'Ameen'); // true
+console.log("teacher == 'Ali'? I predict false");
+console.log(teacher == 'Ali'); // false
+var employee = "Ahmed";
+console.log("employee == 'Ahmed'? I predict true");
+console.log(employee == 'Ahmed'); // true
+console.log("employee == 'Ali'? I predict true");
+console.log(employee == 'Ali'); // false
+var id = 1;
+console.log("id == 1? I predict true");
+console.log(id == 1); // true
+console.log("id == 2? I predict false");
+console.log(id == 2); // false
---
+++ assignment_23.ts
@@ -0,0 +1,43 @@
+/*Q23 Conditional Tests: Write a series of conditional tests. 
+Print a statement describing each test and your prediction for the results of each test. 
+Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. */
+
+let value = 10;
+console.log("value == 10? I predict true");
+console.log(value == 10);// true
+console.log("value == 11? I predict false");
+console.log(value == 11);// false
+
+let student = "Ali";
+console.log("student == 'Ali'? I predict true");
+console.log(student == 'Ali');// true
+console.log("student == 'Ameen'? I predict false");
+console.log(student == 'Ameen');// false
+
+let teacher = "Ameen";
+console.log("teacher == 'Ameen'? I predict true");
+console.log(teacher == 'Ameen');// true
+console.log("teacher == 'Ali'? I predict false");
+console.log(teacher == 'Ali');// false
+
+let employee = "Ahmed";
+console.log("employee == 'Ahmed'? I predict true");
+console.log(employee == 'Ahmed');// true
+console.log("employee == 'Ali'? I predict true");
+console.log(employee == 'Ali');// false
+
+let id = 1;
+console.log("id == 1? I predict true");
+console.log(id == 1);// true
+console.log("id == 2? I predict false");
+console.log(id == 2);// false
