116f46c64e40e9d23620f5eaf07ca73044720f16
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-26 13:03:09 +0500

    Add files via upload

---
+++ assignment_41.js
@@ -0,0 +1,8 @@
+//Q41 Magicians: Make a array of magician’s names. 
+//Pass the array to a function called show_magicians(), 
+//which prints the name of each magician in the array.'
+var magician_names = ["Ali Baba", "Zohaib", "Jawad"];
+function show_magicians(magicians) {
+    console.log(magicians);
+}
+show_magicians(magician_names);
