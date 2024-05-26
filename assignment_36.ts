3ca465902f563eca5520f924d06b8933475f8c5a
Alihussain787 <160561117+Alihussain787@users.noreply.github.com>
2024-05-26 09:57:59 +0500

    Add files via upload

---
+++ assignment_36.ts
@@ -0,0 +1,9 @@
+// Q36 T-Shirt: Write a function called make_shirt() that accepts a size 
+//and the text of a message that should be printed on the shirt. 
+//The function should print a sentence summarizing the size of the shirt 
+//and the message printed on it. Call the function.
+
+function make_shirt(size:string, text:string){
+    console.log(`Size of short is : ${size} and print on shirt : ${text}`);
+}
+make_shirt("large","I love pakistan");
